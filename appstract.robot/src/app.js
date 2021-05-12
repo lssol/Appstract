6f7713e8d2db107d9f8f0bbd8045d7f07bf05379
Sacha Brisset <sacha.brisset@hotmail.fr>
2021-05-12 09:16:47 +0200

    error handling

--- appstract.robot/src/app.js
+++ appstract.robot/src/app.js
@@ -6,13 +6,23 @@ const app = express()
 app.use(cors())
 const port = 3000
 
-app.get('/urltohtml', async (req, res) => {
+app.get('/urltohtml', async (req, res, next) => {
+  const url = req.query.url
+  if (!url) {
+    console.log('No url provided')
+    res.status(400)
+    res.json({error: 'No url provided'})
+    next()
+  }
+  console.log("Retrieving html for url", url)
+
   try {
     const html = await urlToHtml(req.query.url)
     res.json({html})
   }
   catch (e) {
     console.log(e)
+    res.status(500)
     res.json({error: e.toString()})
   }
 })
